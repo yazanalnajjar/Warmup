ff4e06bf1942ac67f7b15e95b0e5c4b69a210fa9
odayabueid <odayabueid1992m@gmail.com>
2019-03-17 10:10:00 +0200

    “averageAge”

--- averageAge.js
+++ averageAge.js
@@ -12,6 +12,17 @@
  ]; 
  function averageAge(people) { 
        // YOUR CODE HERE 
+
+       var sum = 0 ;
+       var length =0;
+       for(var i = 0 ; i<people.length ; i++){
+       	if(people[i].age > 18 && people[i].age <50){
+       		sum = sum + people[i].age;
+       		length = length +1}
+
+       }
+
+       return sum / length;
  }
 
  averageAge(people); // 43+36+44+24 = 36.74
