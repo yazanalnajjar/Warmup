6d145bf5369ae6fd2389e9e8103529c41355dfef
RaedShihab <raedshihab92@gmail.com>
2019-03-17 10:13:25 +0200

    solving

--- averageAge.js
+++ averageAge.js
@@ -1,4 +1,49 @@
-// Write a function called  averageAge that accpets an array of objects and return the average ages for the people who are between 18 and 50
+
+// use the following helper functions in your solution
+
+function each(coll, f) {
+	if (Array.isArray(coll)) {
+		for (var i = 0; i < coll.length; i++) {
+			f(coll[i], i);
+		}
+	} else {
+		for (var key in coll) {
+			f(coll[key], key);
+		}
+	}
+}
+
+function filter(array, predicate) {
+	var acc = [];
+	each(array, function(element, i) {
+		if (predicate(element, i)) {
+			acc.push(element);
+		}
+	});
+	return acc;
+}
+
+function map(array, func) {
+	var acc = [];
+	each(array, function(element, i) {
+		acc.push(func(element, i));
+	});
+	return acc;
+}
+function reduce(array, f, acc) {
+  if (acc === undefined) {
+    acc = array[0];
+    array = array.slice(1);
+  }
+  each(array, function(element, i) {
+    acc = f(acc, element, i);
+  });
+  return acc;
+}
+
+// Write a function called  averageAge that accpets
+// an array of objects and return the average ages for the 
+//people who are between 18 and 50
  
 
  var people = [ 
@@ -11,7 +56,10 @@
        {name: {first: 'Obada', last: 'Eddin'}, age: 24} 
  ]; 
  function averageAge(people) { 
-       // YOUR CODE HERE 
+       var result= reduce(people,function(element){
+       return	element.age>18 && element.age<80
+       })
+       return result/people.length
  }
 
  averageAge(people); // 43+36+44+24 = 36.74
