ae56176f5b6726f85502250eab31fce35e86f510
Yazan <yazannjjar@gmail.com>
2019-03-19 18:01:04 +0200

    Solved

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -4,5 +4,43 @@
 	// 25 ===> 30
 	// 37 ===> 40
  function closestMultipleOf10(num) {
- 	//your code is here
+	 //your code is here
+
+	 return filter(num , function(n){
+		return Math.round(n);
+	 });
+
  }
+
+ function filter(array , predicate) {
+	 var acc = [];
+	 each(array, function(element){
+		if(predicate(element)) {
+			acc.push(element);
+		}
+	 });
+ }
+
+
+
+ function each(coll, func) { 
+	if (Array.isArray(coll)) { 
+		  for (var i = 0; i < coll.length; i++) { 
+				func(coll[i], i); 
+		  } 
+	} else { 
+		  for (var key in coll) { 
+				func(coll[key], key); 
+		  } 
+	} 
+}
+
+
+function closestMultipleOf10(num) {
+	//your code is here
+
+	return filter(num , function(n){
+	   return Math.round(n);
+	});
+
+}
