156fc6cfc1c808850a3ed06906d959f3e836f5b5
Yazan <yazannjjar@gmail.com>
2019-03-20 16:08:00 +0200

    solved

--- strings.js
+++ strings.js
@@ -2,6 +2,15 @@
 // string string exactly n times.
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
-	function repeatStr (n, s) {
-		//your code is here
+function repeatStr (n, s) {
+    //your code is here
+   	var str = "";
+        for(var i = 0 ; i < n; i++){
+       str = str + s;
 	}
+	return str;
+        }
+        //
+        //Sorry i Forgot the syntax 
+    
+    
