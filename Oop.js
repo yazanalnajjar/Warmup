c1f26d9a7459cc02cde78836e9fb96a6f7e29361
Yazan <yazannjjar@gmail.com>
2019-03-25 09:45:43 +0200

    solved

--- Oop.js
+++ Oop.js
@@ -2,29 +2,37 @@
 // Refactor the code as an MakeGame class that shares its methods across different instances.
 
 
-function randInt(n) {
-    return Math.floor(Math.random() * (n + 1));
-}
+
 
 function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
+	var Obj = {};
+	Obj.upperbound = upperbound;
+  Obj.rand=randInt(upperbound);
   var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
-  }
+  return  Obj
+  	
+  
+  	
+  
+}
+ var  guessMyNumber =function(num){
+	var access = this;
+	access.counter++;
+	if (num > access.upperbound) {
+		return "Out of bounds! Please try a number between 0 and " + access.upperbound + ".";
+	} else if (num === access.rand) {
+		return "You guessed my number!";
+	}
+	return "Nope! That wasn't it!";
+}
+
+var giveUp =function (){
+	return access.rand;
+}
+var numOfGuesses = function (){
+	return access.counter;
+}
+
+randInt = function (n) {
+	return Math.floor(Math.random() * (n + 1));
 }
